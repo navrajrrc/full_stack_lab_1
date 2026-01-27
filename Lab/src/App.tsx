423d59cad43435fe15601fbc3cc8f0c64add45cd
Navraj Singh <navrajsingh@rrc.ca>
2026-01-26 21:40:42 -0600

    Lab 1.2

---
+++ Lab/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/header';
+import EmployeeDirectory from './components/employee';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <EmployeeDirectory />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ Lab/src/components/header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <img src="https://itsm-ace.ca/images/logo.svg" alt="Pixell River Logo" style={{height: 60, marginRight: 16}} />  
+            <div>
+                <h1>Pixell River Employee Directory</h1>
+            <p>Welcome to the staff and organization portal!</p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
